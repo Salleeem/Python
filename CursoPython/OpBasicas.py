385a8a48e666c37cf6bf737c532bf9a3ffeb1b28
Salleeem <mateusonego@gmail.com>
2024-01-31 16:16:28 -0300

    !

---
+++ CursoPython/OpBasicas.py
@@ -0,0 +1,30 @@
+# Definições de variavéis
+
+nome = "Alice"
+idade = 25
+
+#Imrpimindo informações
+
+print(f"Olá, {nome}! Você tem {idade} anos.")
+
+
+# Criando uma lista
+
+frutas = ["Maçã", "Banana", "Uva"]
+
+# Iterando sobre a lista
+
+for fruta in frutas:
+    print(fruta)
+
+
+# Definindo uma função
+    
+def saudacao(nome):
+    return f"Olá, {nome}!"
+
+# Chamando a função
+
+mensagem = saudacao("Carlos")
+print(mensagem)
+
